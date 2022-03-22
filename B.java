953d4abfc940f133a1f329b135fd558786f68740
Aditya Padha <81345889+sharluk@users.noreply.github.com>
2022-03-22 10:17:59 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,8 @@
+class B{
+	public static void main(String[] args){
+		System.out.println("name: " + args[0]);
+		System.out.println("course" + args[1]);
+		int a = Integer.parseInt(args[2]);
+		System.out.println("roll no.:" + a);
+	}
+}
